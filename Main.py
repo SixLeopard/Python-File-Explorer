9b392eb703b391c3da3c47c04faee16fd19611bb
Jamie Westerhout <Jtwesterhout@outlook.com>
2022-01-02 16:43:04 +1000

    Added File Viewer

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 
 import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Scrollbar, Toplevel, ttk
 TK_SILENCE_DEPRECATION=1
 
 root = tk.Tk()
@@ -18,7 +18,9 @@ def clear(buttons):
 def list(path, root):
     buttons = []
     directory = []
+    Files = []
     counter = 0
+    filecounter = 0
     currentdir = os.listdir(path)
     column = 0
     row = 0
@@ -40,7 +42,9 @@ def list(path, root):
     #print(directory)
     for filelocations in currentdir:
         if os.path.isfile(path + filelocations):
-            buttons.append(tk.Button(master = root, text=filelocations))
+            Files.append(path + locations)
+            buttons.append(tk.Button(master = root, text=filelocations, command=lambda filecounter=filecounter: OpenFile(Files[filecounter], root)))
+            filecounter += 1
             #print(locations)
 
     for button in buttons:
@@ -58,6 +62,19 @@ def list(path, root):
         print(path)
         list(path, root)
     
+    def OpenFile(path, root):
+        f = open(path, "r")
+        FileWindow = Toplevel(root)
+        FileWindow.grid_columnconfigure(0, weight=1)
+        FileWindow.grid_rowconfigure(0, weight=1)
+        Content = tk.Text(FileWindow)
+        Content.grid(row=0, column=0, sticky='ewns')
+        Content.insert(tk.END, f.read())
+        Scrollbar = tk.Scrollbar(FileWindow, orient="vertical", command=Content.yview)
+        Scrollbar.grid(row=0, column=1, sticky='ns')
+        Scrollbar.config(command=Content.yview)
+        Content.config(yscrollcommand = Scrollbar.set)
+
     #Clear = tk.Button(master = root, text="clear", command=clears)
     #Clear.pack()
 
